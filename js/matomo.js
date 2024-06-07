1537aebf77cba6b1f6470e40bf50f42a972cb772
t0r3tto <thetoretto+0@protonmail.com>
2024-06-07 14:23:17 +0100

    refactor: improved code modularity

---
+++ js/matomo.js
@@ -0,0 +1,12 @@
+var _paq = window._paq = window._paq || [];
+_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+_paq.push(["setCookieDomain", "*.toretto.dev"]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+var u="//toretto.dev/matomo/";
+_paq.push(['setTrackerUrl', u+'matomo.php']);
+_paq.push(['setSiteId', '1']);
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();
